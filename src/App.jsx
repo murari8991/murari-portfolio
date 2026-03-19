69b068e02a3df3580babf16dc01108d314bf5d7b
murari8991 <murari2lakshman.ponduru@gmail.com>
2026-03-19 14:50:27 -0500

    Added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import "./App.scss";
 function App(){
   return (
-    <div class="wrapper">
+    <div class="page">
+    <content class="wrapper">
       <div class="content-card">
-        <p class="my-name">Murari</p>
+        <p class="my-name">Murari Lakshman</p>
       </div>
-      <div class="typewriter">
+      <div class="typewriter content-card">
         <span class="placeholder"></span>
       </div>
+    </content>
+    <content class="wrapper">
+      <div class="content-card">
+        <div>
+          <p class="side-head">About me...</p>
+          <div class="">
+          <h1 class="my-name">Hello, you can call me <span>Murari</span>!</h1>
+          <p>
+            I am a passionate about learning new things in technology and applying it in real-life.
+            Working on projects and building applications that provide value to people is what fuels me. 
+            I am currently pursuing Masters in Information Systems at Saint Louis University.
+            My goal is to be a developer, watching and contributing to the everchanging technology.
+          </p>
+          </div>
+        </div>
+      </div>
+    </content>
     </div>
   )
 }
